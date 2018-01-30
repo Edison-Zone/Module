2273d4ba31c165989ab0e01acfec2413921b5217
Cscherban <christopher.scherban@gmail.com>
2018-01-29 23:37:46 -0500

    characteristic tests

--- ModuleSwitch/ModuleSwitch.ino
+++ ModuleSwitch/ModuleSwitch.ino
@@ -4,11 +4,11 @@
 #define SIGNAL A0
 
 
-BLEPeripheral blePeripheral;  
+BLEPeripheral blePeripheral = BLEPeripheral();  
    
 BLEService servoService(" 19B10000-E8F2-537E-4F6C-D104768A1214");
-BLECharacteristic reader("19B10000-E8F2-537E-4F6C-D104768A1214", BLEWrite | BLERead | BLENotify,1);
-BLECharacteristic spinNum("19B10001-E8F2-537E-4F6C-D104768A1214", BLEWrite | BLERead | BLENotify,1);
+BLEFixedLengthCharacteristic reader("19B10000-E8F2-537E-4F6C-D104768A1214", BLEWrite | BLERead | BLENotify,10);
+BLEFixedLengthCharacteristic spinNum("19B10001-E8F2-537E-4F6C-D104768A1214", BLEWrite | BLERead | BLENotify,10);
 
 void setup() {
   Serial.begin(9600);
@@ -30,7 +30,7 @@ void setup() {
 
   blePeripheral.begin();  //activate BLE device to continuosly transmit BLE advertising packets.
   //Your board will be visible to central devices until it receives a new connection
-  Serial.println("Bluetooth device active, waiting for connections...");
+ 
 
 }
 
@@ -40,12 +40,17 @@ void loop() {
   BLECentral central = blePeripheral.central();   // listen for BLE peripherals to connect
 
   if (central) {                             //when connected to a central
-    Serial.print("Connected to central: ");
-    Serial.println(central.address());  
+     
     while(central.connected()){
       if(reader.written()){
         if(reader.value()){
-          spinNum.setValue(reader.value() + 1) //write the value + 1 to the other one
+          const unsigned char* chars = reader.value();
+          char secondChars[10]; 
+          for (int i = 0; i < 10; i++){
+              secondChars[i] = (char)(chars[i] + 1);
+          }
+          
+          spinNum.setValue(secondChars); //write the value + 1 to the other one
         }
       }
       
@@ -53,3 +58,5 @@ void loop() {
  
   }
 }
+
+

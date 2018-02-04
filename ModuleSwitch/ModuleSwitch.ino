250708865186e00a6bb6824864005e08212dab1a
Cscherban <christopher.scherban@gmail.com>
2018-02-04 13:23:02 -0500

    added some EdisonLang Stuff, AES stuff finished

--- ModuleSwitch/ModuleSwitch.ino
+++ ModuleSwitch/ModuleSwitch.ino
@@ -1,6 +1,6 @@
 #include <CBC.h>
 #include <AES.h>
-
+#include <Servo.h>
 #include <BLEPeripheral.h>
 #define SIGNAL A0
 
@@ -11,8 +11,13 @@ int DATA_TRANSFER_SIZE = 32;
   
 BLEService servoService("19B10000-E8F2-537E-4F6C-D104768A1214");
 BLEFixedLengthCharacteristic reader("19B10000-E8F2-537E-4F6C-D104768A1214", BLEWrite | BLERead | BLENotify, DATA_TRANSFER_SIZE);
-BLEFixedLengthCharacteristic spinNum("19B10001-E8F2-537E-4F6C-D104768A1214", BLEWrite | BLERead | BLENotify, DATA_TRANSFER_SIZE);
+//BLEFixedLengthCharacteristic spinNum("19B10001-E8F2-537E-4F6C-D104768A1214", BLEWrite | BLERead | BLENotify, DATA_TRANSFER_SIZE);
+Servo myservo;
 
+
+
+uint8_t stack[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+size_t stack_size = 0;
 uint8_t iv[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 size_t len = 16;
@@ -22,10 +27,6 @@ void setup() {
   Serial.begin(9600);
   Serial.println("START PROG");
 
-  char string[] = "0118999881999119725 3";
-  uint8_t* original = (uint8_t*)string;
-  int numBytes = strlen(string);
-
   blePeripheral.setLocalName("HomeModesModule1"); // Set a local name for the BLE device
 
   blePeripheral.setAdvertisedServiceUuid(servoService.uuid());
@@ -33,41 +34,62 @@ void setup() {
   // add service and characteristic:
   blePeripheral.addAttribute(servoService);
   blePeripheral.addAttribute(reader);
-  blePeripheral.addAttribute(spinNum);
-
+ 
   // set the initial value for the characeristic:
-  spinNum.setValue(0);
   reader.setValue(0);
 
-  uint8_t* output = encrypt(original, numBytes);
-  uint8_t  thing[DATA_TRANSFER_SIZE];
-  for(int i = 0; i < DATA_TRANSFER_SIZE; i++){
-    thing[i] = output[i];
-  }
-  uint8_t *output2 = decrypt(thing);
   
-  Serial.print("[");
-  for (int i = 0; i < numBytes; i++) {
-    if (i != 0) {
-      Serial.print(", ");
-    }
-    Serial.print((char)original[i]);
-  }
-  Serial.println("]");
-  Serial.print("[");
-  for (int i = 0; output2[i] != 0; i++) {
-    if (i != 0) {
-      Serial.print(", ");
-    }
-    Serial.print((char)output2[i]);
-  }
-  Serial.println("]");
- 
   
   blePeripheral.begin();  //activate BLE device to continuosly transmit BLE advertising packets.
   //Your board will be visible to central devices until it receives a new connection
  
 
+}
+
+
+
+void processReader(){
+   uint8_t *output2 = decrypt(reader);
+   int messageID = output[0]*256*256 + output[1]*256 + output[2]; 
+   int messageLength = 256 * output[4] + output[5];
+
+   
+   
+   for (int i = 6; output2[i] != 255; i++) {
+    uint8_t code = output[i];
+    switch (code) {
+      case 0:
+      return;
+      case 1:
+      //ignore for now
+      break;
+      case 2:
+        stack[stack_size++] = output[++i];
+      break;
+      case 3:
+        stack[stack_size++] = output[++i];
+        stack[stack_size++] = output[++i];
+      break;
+      case 5:
+        servo.write(stack[--stack_size]);
+        stack[stack_size] = 0;
+      break;
+      case 11:
+       delay(stack[--stack_size]);
+       stack[stack_size] = 0;
+      break;
+      case 12:
+       delay(stack[stack_size-2] * 256 + stack[stack_size -1]);
+       stack[stack_size-1] = 0;
+       stack[stack_size-2] = 0;
+       stack_size -= 2;
+      break;
+    }
+   }
+   
+
+
+   
 }
 
 
@@ -80,13 +102,7 @@ void loop() {
     while(central.connected()){
       if(reader.written()){
         if(reader.value()){
-          const unsigned char* chars = reader.value();
-          char secondChars[16]; 
-          for (int i = 0; i < 16; i++){
-              secondChars[i] = (char)(chars[i] + 1);
-          }
-          
-          spinNum.setValue(secondChars); //write the value + 1 to the other one
+          processReader()
         }
       }
       
@@ -113,15 +129,6 @@ uint8_t* encrypt(uint8_t* input, int numBytes){
     for (int i = numBytes; i < DATA_TRANSFER_SIZE; i++) {
       val[i] = DATA_TRANSFER_SIZE - numBytes;
     }
-    Serial.println(sizeof(input)/sizeof(uint8_t));
-  Serial.print("[");
-  for (int i = 0; i < sizeof(val)/sizeof(uint8_t); i++) {
-    if (i != 0) {
-      Serial.print(", ");
-    }
-    Serial.print(val[i]);
-  }
-  Serial.println("]");
     
     uint8_t output[DATA_TRANSFER_SIZE];
     for(int i = 0; i < DATA_TRANSFER_SIZE; i++){
@@ -156,7 +163,7 @@ uint8_t* decrypt(uint8_t* input){
     for (int i = 0; i < DATA_TRANSFER_SIZE - padding; i++) {
       depadded[i] = output[i];
     }
-    depadded[DATA_TRANSFER_SIZE - padding] = 0;
+    depadded[DATA_TRANSFER_SIZE - padding] = 255;
 
     return depadded;
 }

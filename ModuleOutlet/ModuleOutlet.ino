d3ff476c583649179c1742b68a0ae386ca8761ad
Cscherban <christopher.scherban@gmail.com>
2018-02-07 00:17:46 -0500

    Initial commit and some number tweaking

--- ModuleOutlet/ModuleOutlet.ino
+++ ModuleOutlet/ModuleOutlet.ino
@@ -4,16 +4,22 @@
 #include <BLEPeripheral.h>
 #define SIGNAL A0
 
+//An Array to hold all of the pins we care about
+int pins[8] = {0,1,2,3,4,5,6,7};
 
+//BLE peripheral object
 BLEPeripheral blePeripheral = BLEPeripheral();  
- 
-int DATA_TRANSFER_SIZE = 16;
-  
-BLEService servoService("19B10000-E8F2-537E-4F6C-D104768A1214");
 
+ //size of the data being recieved
+int DATA_TRANSFER_SIZE = 16;
+
+//what is being seen as a service
+BLEService outletService("19B10000-E8F2-537E-4F6C-D104768A1214");
+
+//IO channels
 BLEFixedLengthCharacteristic reader("19B10000-E8F2-537E-4F6C-D104768A1214", BLEWrite | BLERead | BLENotify, DATA_TRANSFER_SIZE);
 BLEFixedLengthCharacteristic writer("19B10001-E8F2-537E-4F6C-D104768A1214", BLEWrite | BLERead | BLENotify, DATA_TRANSFER_SIZE);
-Servo myservo;
+
 
 
 
@@ -24,17 +30,15 @@ uint8_t iv[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 size_t len = 16;
 uint8_t key[16] = {0x75, 0x71, 0xac, 0x7a, 0x6b, 0xe3, 0x2f, 0xc8, 0x17, 0xb4, 0xb8, 0x17, 0x63, 0xa7, 0xee, 0xfe};
 
-int var = 90;
 
 void setup() {
   Serial.begin(9600);
-  myservo.attach(2);
-  blePeripheral.setLocalName("HomeModesModule1"); // Set a local name for the BLE device
+  blePeripheral.setLocalName("HomeModesModule2"); // Set a local name for the BLE device
 
-  blePeripheral.setAdvertisedServiceUuid(servoService.uuid());
+  blePeripheral.setAdvertisedServiceUuid(outletService.uuid());
 
   // add service and characteristic:
-  blePeripheral.addAttribute(servoService);
+  blePeripheral.addAttribute(outletService);
   blePeripheral.addAttribute(reader);
   blePeripheral.addAttribute(writer);
  
@@ -51,7 +55,8 @@ void setup() {
 
 }
 
-
+//Processes everythign we care about
+//Reads EdisonLang and implements it
 
 void processReader(uint8_t* input){
   digitalWrite(BLE_LED, LOW);
@@ -60,7 +65,10 @@ void processReader(uint8_t* input){
    int messageID = output[0]*256*256 + output[1]*256 + output[2]; 
    int messageLength = 256 * output[4] + output[5];
    unsigned long startTime;
-   for (int i = 6; i < outSize; i++) {
+   byte bitCompareTo;
+   
+   byte var = 0;
+   for (int j,i = 6; i < outSize; i++) {
     uint8_t code = output[i];
     int waitCount;
     switch (code) {
@@ -77,38 +85,41 @@ void processReader(uint8_t* input){
         stack[stack_size++] = output[++i];
       break;
       case 5:
-        var = (180 * ((int) stack[--stack_size]))/255;
+        var = stack[--stack_size];
         stack[stack_size] = 0;
+        bitCompareTo = 1;
+        for(j= 0; j < sizeof(pins)/sizeof(int); j++){
+          if(var & bitCompareTo != 0){
+            digitalWrite(pins[i],HIGH);
+          }
+          else{
+            digitalWrite(pins[i],LOW);
+          }
+          bitCompareTo *= 2;
+        }
+       
       break;
       case 11:
         waitCount = stack[--stack_size]/15;
-        for (int i = 0; i < waitCount; i++) {
-          myservo.write(var);
-          delay(15);
-        }
-      
-       stack[stack_size] = 0;
+        delay(waitCount);
+        stack[stack_size] = 0;
       break;
       case 12:
        waitCount = (stack[stack_size-2] * 256 + stack[stack_size -1])/15;
-       for (int i = 0; i < waitCount; i++) {
-          myservo.write(var);
-          delay(15);
-        }
+       delay(waitCount);
        stack[stack_size-1] = 0;
        stack[stack_size-2] = 0;
        stack_size -= 2;
       break;
     }
-
-    myservo.write(var);
    }
    
 
 
    
 }
-
+//Loop for stuffs
+//Does everyhitng in the program
 
 void loop() {
 
@@ -117,8 +128,6 @@ void loop() {
   if (central) {                             //when connected to a central
      
     while(central.connected()){
-      myservo.write(var);
-      
       if(reader.written()){
         if(reader.value()){
           digitalWrite(BLE_LED, HIGH);
@@ -148,6 +157,10 @@ void loop() {
   
 }
 
+
+//Yay encryption. 
+//Takes in an input, and the numByes to encrypt
+//Encrypts them
 uint8_t* encrypt(uint8_t* input, int numBytes){
     
     uint8_t val[DATA_TRANSFER_SIZE];
@@ -171,6 +184,9 @@ uint8_t* encrypt(uint8_t* input, int numBytes){
     return output;
 }
 
+
+
+// A rather dumb function to decrypt data
 int decrypt(uint8_t* input, uint8_t* store){
     uint8_t other[DATA_TRANSFER_SIZE];
     for(int i = 0; i < DATA_TRANSFER_SIZE; i++){
